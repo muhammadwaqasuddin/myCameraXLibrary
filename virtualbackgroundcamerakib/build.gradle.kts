6287975a9cf7dc7dba2b07df601e3c6fbdeff208
waqassoftmaticsolutions <muhammad.waqas@softmatic.io>
2025-11-20 19:55:46 +0500

    v1.0.4
    v1.0.4

--- virtualbackgroundcamerakib/build.gradle.kts
+++ virtualbackgroundcamerakib/build.gradle.kts
@@ -1,23 +1,10 @@
-//plugins {
-//    id("java-library")
-//    alias(libs.plugins.jetbrains.kotlin.jvm)
-//
-//}
-//
-//java {
-//    sourceCompatibility = JavaVersion.VERSION_11
-//    targetCompatibility = JavaVersion.VERSION_11
-//}
-//kotlin {
-//    compilerOptions {
-//        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-//    }
-//}
 plugins {
     id("com.android.library")
     kotlin("android")
     kotlin("kapt")
+    id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("maven-publish") // Add this
 }
 
 android {
@@ -50,10 +37,10 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.ui.tooling.preview)
 
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.57.2")
+    // Hilt - Use 'api' so consumers get the dependency
+    api("com.google.dagger:hilt-android:2.57.2")
     kapt("com.google.dagger:hilt-android-compiler:2.57.2")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    api("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // CameraX
     implementation("androidx.camera:camera-core:1.5.0")
@@ -65,9 +52,23 @@ dependencies {
     implementation("com.google.mlkit:segmentation-selfie:16.0.0-beta6")
 
     // Timber
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("com.jakewharton.timber:4.7.1")
 
     // ExoPlayer
     implementation("androidx.media3:media3-exoplayer:1.3.0")
     implementation("androidx.media3:media3-ui:1.3.0")
+}
+
+// Add this for JitPack publishing
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.muhammadwaqasuddin"
+                artifactId = "virtualbackgroundcamerakib"
+                version = "1.0.4"
+            }
+        }
+    }
 }
