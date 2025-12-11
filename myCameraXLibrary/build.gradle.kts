6ae1edbf125e08e1b2e264f5912998c71dc06c5b
waqassoftmaticsolutions <muhammad.waqas@softmatic.io>
2025-12-11 19:49:39 +0500

    downgrade kotlin version

--- myCameraXLibrary/build.gradle.kts
+++ myCameraXLibrary/build.gradle.kts
@@ -87,7 +87,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.muhammadwaqasuddin"
                 artifactId = "mycameraxlibrary"
-                version = "1.1.1"
+                version = "1.1.2"
             }
         }
     }
